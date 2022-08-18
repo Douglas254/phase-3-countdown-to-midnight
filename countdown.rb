57d2726f89e150fc7d9211b03dc49f6aeb25cc9d
Douglas Obara <douglasobara97@gmail.com>
2022-08-18 08:41:16 +0300

    complete assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
+#countdown
+# outputs '<n> SECOND(S)!' string to STDOUT for each count
+# returns HAPPY NEW YEAR!
+def countdown seconds_to_midnight
+    while seconds_to_midnight > 0
+        puts "#{seconds_to_midnight} SECOND(S)!"
+        seconds_to_midnight -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
 
-def countdown
+# countdown_with_sleep
+# should take at least 5 seconds to execute
+def countdown_with_sleep seconds_to_midnight
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    sleep(5)
+    seconds_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
